7592ea2df8525f94bff41a99419ae2d0e2c54310
YagoTaguchi <yagotaguchi@outlook.com>
2024-01-02 22:25:09 -0300

    rightSideBar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
-import LeftSideBar from "@/components/shared/navbar/LeftSideBar";
+import LeftSideBar from "@/components/shared/LeftSideBar";
 import Navbar from "@/components/shared/navbar/Navbar";
+import RightSideBar from "@/components/shared/RightSideBar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -11,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className=" background-light900_dark200 light-border sticky left-0 top-0 flex h-screen min-h-screen w-fit flex-1  flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 max-md:pb-14 max-sm:hidden sm:px-14 lg:w-[266px] dark:shadow-none">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSidebar
+        <RightSideBar />
       </div>
       Toaster
     </main>
---
+++ components/shared/RightSideBar.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import RenderTag from "./RenderTag";
+
+const hotQuestions = [
+  { _id: 1, title: "How do I use express as a custom server in NextJS" },
+  { _id: 2, title: "Cascading Deletes in SQLAlchemy" },
+  { _id: 3, title: "How to perfectly Center a Div with Tailwind CSS" },
+  {
+    _id: 4,
+    title:
+      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+  },
+  { _id: 5, title: "Async/Await Function Not Handling Errors Properly" },
+];
+
+const popularTags = [
+  { _id: 1, name: "javascript", totalQuestions: 5 },
+  { _id: 2, name: "typescript", totalQuestions: 17 },
+  { _id: 3, name: "NextJS", totalQuestions: 10 },
+  { _id: 4, name: "React", totalQuestions: 2 },
+  { _id: 5, name: "SQL", totalQuestions: 3 },
+];
+const RightSideBar = () => {
+  return (
+    <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 max-xl:hidden dark:shadow-none">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {hotQuestions.map((question) => (
+            <Link
+              href={`/questions/${question._id}`}
+              key={question._id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">
+                {question.title}
+              </p>
+              <Image
+                src="assets\icons\chevron-right.svg"
+                width={20}
+                height={20}
+                alt="chevron-right"
+                className="invert-colors "
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16 ">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSideBar;
