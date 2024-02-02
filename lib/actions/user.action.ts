4c1088d38972bbb4d2ec37ebbd09f09d745f7fd1
YagoTaguchi <yagotaguchi@outlook.com>
2024-02-02 18:48:24 -0300

    message

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import User from "../database/user.model";
 
 import { connectToDatabase } from "../mongoose";
+import { CreateUserParams, UpdateUserParams } from "./shared.types";
+import Question from "../database/question.model";
 
 export async function getUserById(params: any) {
   try {
@@ -11,6 +14,68 @@ export async function getUserById(params: any) {
     const { userId } = params;
     const user = await User.findOne({ clerkId: userId });
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParms) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Delete user from database
+    // and questions, answers
+
+    // get user questions
+
+    const userQuestionsIDs = await Question.find({
+      author: user._id,
+    }).distinct("_id");
+
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments e etc.
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deletedUser;
   } catch (error) {
     console.log(error);
     throw error;
