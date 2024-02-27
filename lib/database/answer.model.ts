db4a0cd48f40c7806999253c5135502db711278d
YagoTaguchi <yagotaguchi@outlook.com>
2024-02-27 18:08:08 -0300

    Implementing theAnswer Model

--- lib/database/answer.model.ts
+++ lib/database/answer.model.ts
@@ -1,8 +1,22 @@
 import { Schema, models, model, Document } from "mongoose";
 
-export interface IAnswer extends Document {}
+export interface IAnswer extends Document {
+  content: string;
+  question: Schema.Types.ObjectId;
+  author: Schema.Types.ObjectId;
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
 
-const AnswerSchema = new Schema({});
+const AnswerSchema = new Schema({
+  content: { type: String, required: true },
+  question: { type: Schema.Types.ObjectId, required: true, ref: "Question" },
+  author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, default: Date.now },
+});
 
 const Answer = models.Question || model("Answer", AnswerSchema);
 
